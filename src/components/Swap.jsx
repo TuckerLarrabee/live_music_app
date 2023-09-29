6ff932aa4bf08e245602611c263263962d2d176c
tuckl <tuckerlarrabee13@gmail.com>
2023-09-29 15:49:15 -0400

    MVP working, audio files playing on click

---
+++ src/components/Swap.jsx
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightLeft } from "@fortawesome/free-solid-svg-icons";
+import '../styles/SwapIcon.css'
+
+const SwapIcon = () => {
+    return ( 
+        <FontAwesomeIcon id="swap" icon={faRightLeft}></FontAwesomeIcon>
+     );
+}
+ 
+export default SwapIcon;
